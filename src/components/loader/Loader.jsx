680a547b52a9c32b6282175bf36a452702ee5677
Dan Retegan <danretegan@yahoo.com>
2024-01-27 11:53:17 +0200

    Revert "Revert "partial setup""
    This reverts commit 135c5b6b69914223f0c2640233c468c9e43028bc.

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,9 @@
+// components//loader/Loader.jsx
+import React from 'react';
+import styles from './Loader.module.css';
+
+const Loader = () => {
+  return <div className={styles.loader}>Loading...</div>;
+};
+
+export default Loader;
